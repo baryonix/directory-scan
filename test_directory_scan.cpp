056b25f259d3460d8e53a968099e844677190274
Jan Andres <jandres@gmx.net>
2019-07-07 18:07:32 +0200

    Verify file size in PathScanner test.

--- test_directory_scan.cpp
+++ test_directory_scan.cpp
@@ -40,13 +40,15 @@ static const fs::path TEST_ROOT{"TEST_ROOT"};
 static const fs::path TEST_FILE = TEST_ROOT / "file";
 static const fs::path TEST_SUBDIR = TEST_ROOT / "subdir";
 
+static const std::string TEST_FILE_CONTENT = "foobar";
+
 static void setUpTestDirectory() {
     if (fs::exists(TEST_ROOT))
         fs::remove_all(TEST_ROOT);
 
     fs::create_directory(TEST_ROOT);
     fs::create_directory(TEST_SUBDIR);
-    std::ofstream(TEST_FILE) << "foobar" << std::endl;
+    std::ofstream(TEST_FILE) << TEST_FILE_CONTENT;
 }
 
 class MockConsumer {
@@ -82,7 +84,10 @@ TEST(PathScanner, scan) {
     StubbedPathScanner pathScanner{TEST_ROOT, executor, consumer};
 
     EXPECT_CALL(executor, execute(Property(&StubbedPathScanner::getPath, Eq(TEST_SUBDIR))));
-    EXPECT_CALL(consumer, consume(Property(&directory_scan::FileInformation::path, Eq(TEST_FILE))));
+    EXPECT_CALL(consumer, consume(AllOf(
+            Property(&directory_scan::FileInformation::path, Eq(TEST_FILE)),
+            Property(&directory_scan::FileInformation::size, Eq(TEST_FILE_CONTENT.length())))
+    ));
 
     pathScanner();
 }
